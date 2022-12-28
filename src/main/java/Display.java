d71e0b5811ed0e1c5b2eae598d4c69b67a7a0f9f
Kirill Minov <kirillminov@icloud.com>
2022-12-28 22:06:00 +0300

    initialCommit

---
+++ src/main/java/Display.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Display {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JavaGame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
